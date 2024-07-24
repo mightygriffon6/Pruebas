219cefa465427d3708f8861350b1dfa1f7bda37c
Marcelo Andre Guevara Gutierrez <marce_06@outlook.com>
2024-07-24 11:13:48 -0500

    Cambio de estructura del Home / usando clases tarjetas y cursos

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -1,4 +1,7 @@
 ﻿
+using BlazorBootstrap;
+using System.Collections;
+
 namespace Pruebas.Pages
 {
 
@@ -6,6 +9,7 @@ namespace Pruebas.Pages
     {
         private bool showGrid = true;
         private List<Tarjeta> tarjetas;
+        private List<Curso> cursos;
 
         private void ShowGrid()
         {
@@ -24,35 +28,88 @@ namespace Pruebas.Pages
         }
         protected override void OnInitialized()
         {
-            tarjetas = new List<Tarjeta>
-        {
-            new Tarjeta(24.45m, "images/excel.jpg", "Excel Básico", "San Agustin", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
-            new Tarjeta(150, "images/word.png", "Word Intermedio", "Garaycochea", new TimeSpan(14, 0, 0), new TimeSpan(17, 0, 0)),
-            new Tarjeta(80, "images/powerpoint.jpg", "Power Point Basico", "San Agustin", new TimeSpan(18, 0, 0), new TimeSpan(20, 0, 0)),
-            new Tarjeta(150, "images/contable.png", "Programacion contable", "Garaycochea", new TimeSpan(14, 0, 0), new TimeSpan(17, 0, 0)),
-            new Tarjeta(80, "images/photoshop.jpg", "Photshopp Intermedio", "Garaycochea", new TimeSpan(18, 0, 0), new TimeSpan(20, 0, 0)),
-            new Tarjeta(150, "images/SQL.png", "SQL Básico", "San Agustin", new TimeSpan(14, 0, 0), new TimeSpan(17, 0, 0)),
-            new Tarjeta(80, "images/imagen3.jpg", "Curso de Inglés", "San Agustin", new TimeSpan(18, 0, 0), new TimeSpan(20, 0, 0))
-        };
+            cursos = new List<Curso>
+            {
+                new Curso(24.45m, "images/excel.jpg", "Excel Básico", "Emp", "San Agustin", "LMXJV", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
+                new Curso(24.45m, "images/excel.jpg", "Excel Básico", "Emp", "San Agustin", "LMXJV", new TimeSpan(9, 0, 0), new TimeSpan(3, 0, 0)),
+                new Curso(24.45m, "images/excel.jpg", "Excel Básico", "Lib", "San Agustin", "LMXJV", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
+                new Curso(24.45m, "images/excel.jpg", "Excel Básico", "Emp", "Virtual", "LMXJV", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
+                new Curso(50, "images/word.png", "Word Intermedio", "Emp", "Virtual", "LMXJV", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
+                new Curso(24.45m, "images/word.png", "Word Intermedio", "Emp", "Virtual", "LM", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
+                new Curso(24.45m, "images/word.pnhg", "Word Intermedio", "Emp", "Virtual", "LMXJV", new TimeSpan(18, 0, 0), new TimeSpan(12, 0, 0)),
+                new Curso(24.45m, "images/excel.jpg", "Excel Básico", "Emp", "San Agustin", "LMXJV", new TimeSpan(21, 0, 0), new TimeSpan(26, 0, 0)),
+                new Curso(24.45m, "images/excel.jpg", "Excel Básico", "Emp", "Virtual", "LMXJV", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0))
+            };
+            tarjetas = new List<Tarjeta>();
+            foreach (Curso curso in cursos)
+            {
+                // Busca si ya existe una tarjeta con el mismo curso, local y días
+                Tarjeta tarjetaExistente = tarjetas.Find(t => t.Curso == "(" + curso.TipdeCurso + ")" + curso.Descripcion && t.Local == curso.Local && t.Dias == curso.Dias);
+
+                if (tarjetaExistente == null)
+                {
+                    // Si no existe, crea una nueva tarjeta
+                    Tarjeta nuevaTarjeta = new Tarjeta(curso.Costo, curso.Img, "(" + curso.TipdeCurso + ")" + curso.Descripcion, curso.Local)
+                    {
+                        Dias = curso.Dias
+                    };
+                    nuevaTarjeta.AgregarHorario(curso.Inicio, curso.Final);
+                    tarjetas.Add(nuevaTarjeta);
+                }
+                else
+                {
+                    // Si existe, solo agrega el horario
+                    tarjetaExistente.AgregarHorario(curso.Inicio, curso.Final);
+                }
+            }
         }
     }
     public class Tarjeta
     {
-        public decimal costo { get; set; }
-        public string img { get; set; } // Puedes usar string para el nombre de la imagen
-        public string curso { get; set; }
-        public string local { get; set; }
-        public TimeSpan inicio { get; set; }
-        public TimeSpan final { get; set; }
+        public decimal Costo { get; set; }
+        public string Img { get; set; }
+        public string Curso { get; set; }
+        public string Local { get; set; }
+        public string Dias { get; set; }
+        public ArrayList Horarios { get; set; }
 
-        public Tarjeta(decimal costo, string img, string curso, string local, TimeSpan inicio, TimeSpan final)
+        public Tarjeta(decimal costo, string img, string curso, string local)
         {
-            this.costo = costo;
-            this.img = img;
-            this.curso = curso;
-            this.local = local;
-            this.inicio = inicio;
-            this.final = final;
+            Costo = costo;
+            Img = img;
+            Curso = curso;
+            Local = local;
+            Dias = "";
+            Horarios = new ArrayList();
+        }
+
+        public void AgregarHorario(TimeSpan inicio, TimeSpan final)
+        {
+            Horarios.Add(new { Inicio = inicio, Final = final });
+        }
+    }
+
+    public class Curso
+    {
+        public decimal Costo { get; set; }
+        public string Img { get; set; }
+        public string Descripcion { get; set; }
+        public string TipdeCurso { get; set; }
+        public string Local { get; set; }
+        public string Dias { get; set; }
+        public TimeSpan Inicio { get; set; }
+        public TimeSpan Final { get; set; }
+
+        public Curso(decimal costo, string img, string descripcion, string tip, string local, string dias, TimeSpan inicio, TimeSpan final)
+        {
+            Costo = costo;
+            Img = img;
+            Descripcion = descripcion;
+            TipdeCurso = tip;
+            Local = local;
+            Dias = dias;
+            Inicio = inicio;
+            Final = final;
         }
     }
 }
--- Pages/NuevoHome.razor.cs
+++ Pages/NuevoHome.razor.cs
@@ -2,20 +2,7 @@
 {
     public partial class NuevoHome
     {
-        private List<Tarjeta> tarjetas;
-        protected override void OnInitialized()
-        {
-            tarjetas = new List<Tarjeta>
-        {
-            new Tarjeta(24.45m, "images/excel.jpg", "Excel Básico", "San Agustin", new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
-            new Tarjeta(150, "images/word.png", "Word Intermedio", "Garaycochea", new TimeSpan(14, 0, 0), new TimeSpan(17, 0, 0)),
-            new Tarjeta(80, "images/powerpoint.jpg", "Power Point Basico", "San Agustin", new TimeSpan(18, 0, 0), new TimeSpan(20, 0, 0)),
-            new Tarjeta(150, "images/contable.png", "Programacion contable", "Garaycochea", new TimeSpan(14, 0, 0), new TimeSpan(17, 0, 0)),
-            new Tarjeta(80, "images/photoshop.jpg", "Photshopp Intermedio", "Garaycochea", new TimeSpan(18, 0, 0), new TimeSpan(20, 0, 0)),
-            new Tarjeta(150, "images/SQL.png", "SQL Básico", "San Agustin", new TimeSpan(14, 0, 0), new TimeSpan(17, 0, 0)),
-            new Tarjeta(80, "images/imagen3.jpg", "Curso de Inglés", "San Agustin", new TimeSpan(18, 0, 0), new TimeSpan(20, 0, 0))
-        };
-        }
+        
     }
     
 }
